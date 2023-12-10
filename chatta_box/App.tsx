cfd6d9d9ea83f0ddafd5524ea7e0e07097c9c23b
kjordine <kieranjordine@gmail.com>
2023-12-09 23:32:26 -0500

    open ai commit

--- chatta_box/App.tsx
+++ chatta_box/App.tsx
@@ -1,45 +1,122 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  View
+  TextInput
 } from 'react-native';
+import OpenAI from 'openai';
+
+const env = process.env.NODE_ENV;
+const apiKey = process.env.OPENAI_API_KEY;
+const org = process.env.OPENAI_ORG;
+
+const openai = new OpenAI({
+  //https://api.openai.com/v1/chat/completions
+  // baseURL: 'https://api.openai.com/v1',
+  apiKey: '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'
+});
 
 function App() {
-  const env = process.env;
-  const env1 = process.env.NODE_ENV;
-  const api1 = process.env.API_TOKEN;
-  const api2 = process.env.APP_ENV_API_TOKEN;
+  // const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState(
+    'Hello ChatGPT. Calling you from NodsJS.'
+  );
+  const [response, setResponse] = useState({});
+  const [error, setError] = useState('');
+
+  async function chat() {
+    if (message) {
+      try {
+        const chatCompetion = await openai.chat.completions.create({
+          messages: [{role: 'user', content: message}],
+          model: 'gpt-3.5-turbo'
+        });
+        setResponse(chatCompetion);
+        setMessage('');
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+        console.log(e);
+      }
+    }
+  }
+
+  async function manual() {
+    const url = 'https://api.openai.com/v1/chat/completions';
+    const data = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: 'Hello from NodeJS'
+        }
+      ]
+    };
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + apiKey
+        },
+        body: JSON.stringify(data)
+      });
+      setResponse(await response.json());
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e.message);
+      }
+      console.log(e);
+    }
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text style={styles.sectionDescription}>AI Project</Text>
-        <Text>{JSON.stringify(env, null, 2)}</Text>
-        <Text>{env1}</Text>
-        <Text>{api1}</Text>
-        <Text>{api2}</Text>
-        <View></View>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={styles.container}>
+        <Text style={styles.header}>AI Project</Text>
+        <Text>{env}</Text>
+        <Text>{apiKey}</Text>
+        <Text>{org}</Text>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Message"
+          onChangeText={setMessage}
+          onSubmitEditing={chat}
+          value={message}
+        />
+        <Button title="Chat" onPress={chat} />
+        <Button title="Fetch" onPress={manual} />
+        {response && <Text>{JSON.stringify(response, null, 2)}</Text>}
+        {error && <Text>{error}</Text>}
       </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionDescription: {
+  container: {
+    padding: 8
+  },
+  header: {
     marginVertical: 8,
     fontSize: 18,
-    fontWeight: '400'
+    fontWeight: '400',
+    textAlign: 'center'
+  },
+  inputStyle: {
+    borderWidth: 0.51,
+    borderColor: 'grey',
+    padding: 8,
+    borderRadius: 8,
+    marginVertical: 8
   }
 });
 
